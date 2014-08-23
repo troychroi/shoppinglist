45840eb4d9d35897977dec9b05fdadb718ac0692
Troy Church <tchurch@berklee.edu>
2014-08-23 14:30:37 -0400

    fixed checkable items issue
    event delegation for li.checked on click

--- js.js
+++ js.js
@@ -21,30 +21,58 @@ $(document).ready(function() {
 		});
 	} */
 
+	// define dynamically populated list items
+	var listItems = $('#itemsListed').find('li')
+
 	// mark item as checked and add delete functionality 
 	function check() {
-		$('#itemsListed').find('li').on('click', function() {
+		$('#itemsListed').on('click', 'li', function() {	
 			$(this).toggleClass('checked');
 			$(this).append('<hr />');
 			$(this).append('<button>X</button>');
 			$(this).find('button').addClass('trash');
+			$(this).not('.checked').find('hr').remove();
+			$(this).not('.checked').find('button').remove();
 		});
 	}
 
 	// the delete functionality
 	function trash() {
-		$('#itemsListed').find('li').on('click', function() {
-			$('li').not('.checked').find('hr').remove();
-			$('li').not('.checked').find('button').remove();
-			$('.trash').on('click', function() {
-				$(this).closest('li').remove();
-					if (itemNumber >= 1) {
-						itemNumber --;
-					}
-			});
+		$('.checked').on('click', 'button', function() {
+			$(this).closest('li').remove();
+				if (itemNumber >= 1) {
+					itemNumber --;
+				}
 		});
-
 	}
+
+
+
+	// enable add button
+	$('.addButton').on('click', function() {
+		alert($('#itemInput').find('input').val());
+		if ($('#itemInput').find('input').val() != '') {
+			if (enterItem == []) {
+				// push entered items into blank enterItem array
+				enterItem.push($('#itemInput').find('input').val());
+				$('#itemsListed').append('<li>'+ enterItem[itemNumber] +'</li>');
+				$('#itemInput').find('input').val('');
+				check();
+				trash();
+				itemNumber ++;
+				console.log(itemNumber);
+			} else if (itemNumber > 0) {
+				// add entered items into previously populated enterItem array
+				enterItem.push($('#itemInput').find('input').val());
+				$('#itemsListed').prepend('<li>'+ enterItem[itemNumber] +'</li>');
+				$('#itemInput').find('input').val('');
+				check();
+				trash();
+				itemNumber ++;
+				console.log(itemNumber);
+			}
+		}
+	});
 
 	// adding items to the list 
 	$(document).keydown(function(event){
