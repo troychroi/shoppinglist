53115a547d1d224c122fa93c126acb1304bc7935
Troy Church <tchurch@berklee.edu>
2014-08-24 00:54:35 -0400

    trash button
    added delete functionality

--- js.js
+++ js.js
@@ -16,81 +16,70 @@ $(document).ready(function() {
 				if (itemNumber >= 1) {
 					itemNumber --;
 				}
-				console.log(itemNumber);
 			});
 		});
 	} */
 
 	// mark item as checked and add delete functionality 
 	function check() {
-		$('#itemsListed').on('click', 'li', function() {	
+		$('#itemsListed').on('click', 'li', function(event) {	
 			$(this).toggleClass('checked');
 			$(this).append('<hr />');
-			$(this).append('<button>X</button>');
-			$(this).find('button').addClass('trash');
+			$(this).find('hr').animate({'width': '516px'});
+			$(this).append('<button class="trash">X</button>');
+			$(this).find('button').animate({'opacity': '1'});
 			$(this).not('.checked').find('hr').remove();
 			$(this).not('.checked').find('button').remove();
+			// enable trash or delete functionality individual list items
+			$('.trash').on('click', function() {
+				$(this).closest('li').remove();
+			});
 		});
 	}
-
-	// the delete functionality
-	function trash() {
-		$('.checked').on('click', 'button', function() {
-			$(this).closest('li').remove();
-				if (itemNumber >= 1) {
-					itemNumber --;
-				}
-		});
-	}
-
-
 
 	// enable add button
 	$('.addButton').on('click', function() {
-		alert($('#itemInput').find('input').val());
 		if ($('#itemInput').find('input').val() != '') {
-			if (itemNumber <= 0) {
+			if (itemNumber == 0) {
 				// push entered items into blank enterItem array
 				enterItem.push($('#itemInput').find('input').val());
 				$('#itemsListed').append('<li>'+ enterItem[itemNumber] +'</li>');
 				$('#itemInput').find('input').val('');
 				check();
-				trash();
+
+				$('#itemInput').find('input').focus();
 				itemNumber ++;
-				console.log(itemNumber);
 			} else if (itemNumber > 0) {
 				// add entered items into previously populated enterItem array
 				enterItem.push($('#itemInput').find('input').val());
 				$('#itemsListed').prepend('<li>'+ enterItem[itemNumber] +'</li>');
 				$('#itemInput').find('input').val('');
 				check();
-				trash();
+
+				$('#itemInput').find('input').focus();
 				itemNumber ++;
-				console.log(itemNumber);
 			}
 		}
 	});
 
 	// adding items to the list 
 	$(document).keydown(function(event){
-		if (event.which == 13 && enterItem == []) {
-			// push entered items into blank enterItem array
-			enterItem.push($('#itemInput').find('input').val());
-			$('#itemsListed').append('<li>'+ enterItem[itemNumber] +'</li>');
-			$('#itemInput').find('input').val('');
-			check();
-			trash();
-			itemNumber ++;
-			console.log(itemNumber);
-		} else if (event.which == 13) {
-			// add entered items into previously populated enterItem array
-			enterItem.push($('#itemInput').find('input').val());
-			$('#itemsListed').prepend('<li>'+ enterItem[itemNumber] +'</li>');
-			$('#itemInput').find('input').val('');
-			check();
-			trash();
-			itemNumber ++;
-			console.log(itemNumber);
+		if ($('#itemInput').find('input').val() != '') {
+			if (event.which == 13 && enterItem == []) {
+				// push entered items into blank enterItem array
+				enterItem.push($('#itemInput').find('input').val());
+				$('#itemsListed').append('<li>'+ enterItem[itemNumber] +'</li>');
+				$('#itemInput').find('input').val('');
+				check();
+				itemNumber ++;
+			} else if (event.which == 13) {
+				// add entered items into previously populated enterItem array
+				enterItem.push($('#itemInput').find('input').val());
+				$('#itemsListed').prepend('<li>'+ enterItem[itemNumber] +'</li>');
+				$('#itemInput').find('input').val('');
+				check();
+				itemNumber ++;
+			}
 		}
 	});
 });
