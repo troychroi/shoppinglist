dd4415a5848ab152f1e7d580cbe9484af9e63a71
Troy Church <tchurch@berklee.edu>
2014-08-24 20:13:59 -0400

    update shopping list
    fixed delete issue

--- js/js.js
+++ js/js.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	
-	var enterItem = [];
-	var itemNumber = 0; 
+	enterItem = []
+	itemNumber = 0 
 
 
 	// mark item as checked and add delete functionality 
@@ -26,8 +26,8 @@ $(document).ready(function() {
 	// enable add button
 	$('.addButton').on('click', function() {
 		// prevent emptiness being pushed into array
-		if ($('#itemInput').find('input').val() !== '') {
-			if (itemNumber === 0) {
+		if ($('#itemInput').find('input').val() != '') {
+			if (itemNumber == 0) {
 				// push entered items into blank enterItem array
 				enterItem.push($('#itemInput').find('input').val());
 				$('#itemsListed').append('<li>'+ enterItem[itemNumber] +'</li>');
@@ -50,8 +50,8 @@ $(document).ready(function() {
 	// adding items to the list on pressing enter
 	$(document).keydown(function(event){
 		// prevent emptiness being pushed into array
-		if ($('#itemInput').find('input').val() !== '') {
-			if (event.which == 13 && enterItem === []) {
+		if ($('#itemInput').find('input').val() != '') {
+			if (event.which == 13 && enterItem == []) {
 				// push entered items into blank enterItem array
 				enterItem.push($('#itemInput').find('input').val());
 				$('#itemsListed').append('<li>'+ enterItem[itemNumber] +'</li>');
