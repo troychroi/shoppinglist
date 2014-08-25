4538f2644e56bae530000f90dceb9f96674c145b
Troy Church <tchurch@berklee.edu>
2014-08-25 01:13:54 -0400

    mini update

--- js/js.js
+++ js/js.js
@@ -3,6 +3,11 @@ $(document).ready(function() {
 	var enterItem = [];
 	var itemNumber = 0; 
 
+	$(function() {
+    	$( "#itemsListed" ).sortable();
+    	$( "#itemsListed" ).disableSelection();
+  	});
+
 
 	// mark item as checked and add delete functionality 
 		$('#itemsListed').on('click', 'li', function() {	
